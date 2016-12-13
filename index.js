6d106b6401b4a668a60973c0d784c7c52f42b58d
Ricardo Canastro <ricardo.canastro@blip.pt>
2016-12-13 17:09:25 +0000

    Use mem, extract checks into smaller functions and add unit tests

--- index.js
+++ index.js
@@ -1,104 +1,103 @@
 'use strict';
+
+const mem = require('mem');
 const execa = require('execa');
 const passwdUser = require('passwd-user');
-let fullname;
-let first = true;
+const pAny = require('p-any');
 
-function getEnvVar() {
-	const env = process.env;
-
-	return env.GIT_AUTHOR_NAME ||
-		env.GIT_COMMITTER_NAME ||
-		env.HGUSER || // Mercurial
-		env.C9_USER; // Cloud9
-}
-
-module.exports = () => {
-	if (!first) {
-		return Promise.resolve(fullname);
-	}
-
-	first = false;
-
-	if (fullname) {
-		return Promise.resolve(fullname);
-	}
-
-	const envVar = getEnvVar();
-
-	if (envVar) {
-		fullname = envVar;
-		return Promise.resolve(fullname);
-	}
-
-	return Promise.resolve().then(() => {
-		fullname = require('rc')('npm')['init-author-name'];
+function checkEnv() {
+	return new Promise((resolve, reject) => {
+		const env = process.env;
+		const fullname = env.GIT_AUTHOR_NAME ||
+			env.GIT_COMMITTER_NAME ||
+			env.HGUSER || // Mercurial
+			env.C9_USER; // Cloud9
 
 		if (!fullname) {
-			return fallback().then(() => fullname);
+			reject();
+			return;
 		}
 
-		return fullname;
-	}).catch(fallback).then(() => fullname).catch(() => {});
-};
+		resolve(fullname);
+	});
+}
 
-function fallback() {
-	if (process.platform === 'darwin') {
-		return passwdUser()
-			.then(user => {
-				fullname = user.fullname;
-				return fullname;
-			})
-			.catch(() => {
-				return execa.stdout('osascript', ['-e', 'long user name of (system info)']).then(stdout => {
-					fullname = stdout;
-					return fullname;
-				});
-			});
-	}
+function checkAuthorName() {
+	return new Promise((resolve, reject) => {
+		const fullname = require('rc')('npm')['init-author-name'];
 
-	if (process.platform === 'win32') {
-		// try git first since fullname is usually not set by default in the system on Windows 7+
-		return execa('git', ['config', '--global', 'user.name'])
-			.then(res => {
-				fullname = res.stdout;
+		if (!fullname) {
+			reject();
+			return;
+		}
 
-				if (!fullname) {
-					throw new Error();
-				}
-			})
-			.catch(() => {
-				return execa.stdout('wmic', ['useraccount', 'where', 'name="%username%"', 'get', 'fullname'])
-					.then(stdout => {
-						fullname = stdout.replace('FullName', '');
-						return fullname;
-					});
-			});
-	}
+		resolve(fullname);
+	});
+}
 
+function checkPasswd() {
 	return passwdUser()
 		.then(user => {
-			fullname = user.fullname;
+			if (!user.fullname) {
+				throw new Error();
+			}
+
+			return user.fullname;
+		});
+}
+
+function checkGit() {
+	return execa.stdout('git', ['config', '--global', 'user.name'])
+		.then(fullname => fullname || Promise.reject());
+}
+
+function checkOsaScript() {
+	return execa.stdout('osascript', ['-e', 'long user name of (system info)'])
+		.then(fullname => fullname || Promise.reject());
+}
+
+function checkWmic() {
+	return execa.stdout('wmic', ['useraccount', 'where', 'name="%username%"', 'get', 'fullname'])
+		.then(stdout => {
+			const fullname = stdout.replace('FullName', '');
 
 			if (!fullname) {
 				throw new Error();
 			}
-		})
-		.catch(() => {
-			return execa.shell('getent passwd $(whoami)')
-				.then(res => {
-					fullname = (res.stdout.split(':')[4] || '').replace(/,.*/, '');
 
-					if (!fullname) {
-						throw new Error();
-					}
-				});
-		})
-		.catch(() => {
-			return execa.stdout('git', ['config', '--global', 'user.name'])
-				.then(stdout => {
-					fullname = stdout;
-					return fullname;
-				});
+			return fullname;
 		});
 }
+
+function checkGetEnt() {
+	return execa.shell('getent passwd $(whoami)').then(res => {
+		const fullname = (res.stdout.split(':')[4] || '').replace(/,.*/, '');
+		if (!fullname) {
+			throw new Error();
+		}
+
+		return fullname;
+	});
+}
+
+function fallback() {
+	if (process.platform === 'darwin') {
+		return pAny([checkPasswd(), checkOsaScript()]);
+	}
+
+	if (process.platform === 'win32') {
+		// Fullname is usually not set by default in the system on Windows 7+
+		return pAny([checkGit(), checkWmic()]);
+	}
+
+	return pAny([checkPasswd(), checkGetEnt(), checkGit()]);
+}
+
+function getFullName() {
+	return checkEnv()
+		.catch(checkAuthorName)
+		.catch(fallback)
+		.catch(() => {});
+}
+
+module.exports = mem(getFullName);
